fdce0aa6a6dfa9c340f7a118a8c32e4fb21242c0
Lucas Pape <admin@lucaspape.de>
2022-02-25 15:09:53 +0100

    Fix

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const {v4: uuidv4} = require('uuid')
 const webp = require('webp-converter')
-const {fileTypeFromFile} = require('file-type')
+const detectFileType = detect = require('detect-file-type')
 const imageThumbnail = require('image-thumbnail')
 const Permission = require('./permission.js')
 
@@ -144,29 +144,30 @@ module.exports = class Api {
                             file.pipe(writeStream)
 
                             writeStream.on('close', () => {
-                                fileTypeFromFile(TEMP_DIR + tempId).then((type) => {
-                                    if(type.mime === 'image/gif'){
-                                        const imageId = uuidv4() + '.gif'
-
-                                        fs.copyFile(TEMP_DIR + tempId, CONTENT_DIR + imageId, (err) => {
-                                            if(err){
-                                                res.status(500).send({message: 'Internal server error'})
-                                                console.log(err)
-                                            }else{
-                                                this.database.insertImage(session.user.id, imageId, this.config.cdnId).then(() => {
-                                                    res.send({message: 'Animated uploaded', imageId: imageId, cdnId: this.config.cdnId})
-                                                }).catch(e => {
-                                                    res.status(500).send({message: 'Internal server error'})
-                                                    console.log(e)
-                                                })
-                                            }
-                                        })
-                                    }else{
+                                detectFileType.fromFile(TEMP_DIR + tempId, (err, type) => {
+                                    if(err){
                                         res.status(400).send({message: 'Unsupported filetype'})
+                                    }else{
+                                        if(type.mime === 'image/gif'){
+                                            const imageId = uuidv4() + '.gif'
+
+                                            fs.copyFile(TEMP_DIR + tempId, CONTENT_DIR + imageId, (err) => {
+                                                if(err){
+                                                    res.status(500).send({message: 'Internal server error'})
+                                                    console.log(err)
+                                                }else{
+                                                    this.database.insertImage(session.user.id, imageId, this.config.cdnId).then(() => {
+                                                        res.send({message: 'Animated uploaded', imageId: imageId, cdnId: this.config.cdnId})
+                                                    }).catch(e => {
+                                                        res.status(500).send({message: 'Internal server error'})
+                                                        console.log(e)
+                                                    })
+                                                }
+                                            })
+                                        }else{
+                                            res.status(400).send({message: 'Unsupported filetype'})
+                                        }
                                     }
-                                }).catch((e) => {
-                                    res.status(500).send({message: 'Internal server error'})
-                                    console.log(e)
                                 })
                             })
                         } catch (e) {
